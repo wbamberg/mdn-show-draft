4a85689a7a8fe7aa23ae0ddc53269c97347576cd
Will Bamberg <wbamberg@mozilla.com>
2016-03-12 14:33:35 -0800

    grab cached content right away

--- content-script.js
+++ content-script.js
@@ -1,19 +1,22 @@
+var draftKey = "draft/edit" + document.location.pathname.slice(0, -5);
+var draftContent = window.localStorage[draftKey];
 
 function showDraft(e) {
-  var draftKey = "draft/edit" + document.location.pathname.slice(0, -5);
-  var draftContent = window.localStorage[draftKey];
+  if (!draftContent) {
+    return;
+  }
   chrome.runtime.sendMessage({draft: draftContent, url: document.location.href});
   e.preventDefault();
 }
 
-function pageHasCachedDraft(notice) {
+function pageHasCachedDraftNotice(notice) {
   var draftNoticeStart = chrome.i18n.getMessage("noticeStart");
   return (notice && (notice.textContent.indexOf(draftNoticeStart) == 0));
 }
 
 var notice = document.querySelector(".notice");
 
-if (pageHasCachedDraft(notice)) {  
+if (pageHasCachedDraftNotice(notice)) {  
   var showDraftLink = document.createElement("a");
   showDraftLink.textContent = chrome.i18n.getMessage("linkName");
   showDraftLink.addEventListener("click", showDraft);
